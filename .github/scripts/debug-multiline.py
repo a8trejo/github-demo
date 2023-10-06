32f9149d0c4b2eae4f28359036d33cde1d76733e
a8trejo <abel.trejo@roo.vet>
2023-10-06 15:52:17 -0700

    Testing writing multiline string within python script

---
+++ .github/scripts/debug-multiline.py
@@ -0,0 +1,8 @@
+import os
+
+GITHUB_ENV = os.getenv('GITHUB_ENV')
+MULTILINE_STRING = ":loud_sound: Woo-Hoo :loud_sound: ANALYSIS SUCCESSFUL, 1 more details at: ***/dashboard?\n*Security Hotspots on New Code*: :warning: 0\n*Maintainability Rating on New Code: OK* :cat-on-keyboard: Code Smells: 0\n*Reliability Rating on New Code: OK* :bug: Bugs: 0\n*Security Rating on New Code: OK* :unlock: Vulnerabilities: 0\n"
+
+print(MULTILINE_STRING)
+with open(GITHUB_ENV, "a") as env_file:
+    env_file.write(f"VAR_NAME<<EOF{MULTILINE_STRING}EOF")
